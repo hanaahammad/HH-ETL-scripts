13a85a35aa8d8acbd71423456a95004d2f2757b7
Hanaa Hammad <34770828+hanaahammad@users.noreply.github.com>
2025-04-23 15:08:15 +0200

    changing project hierarchy
    added a folder and a changed the location of a file
    
    Co-Authored-By: hanaa <92865+hanaa@users.noreply.github.com>

--- concat_util.py
+++
@@ -1,6 +0,0 @@
-def concat_4(A, B, C, D ):
-    return f"'{A}',  '{B}', '{C}', '{D}'"
-
-#print(concat('apple','banana', 'pear'))
-def concat_2(A, B ):
-    return f"'{A}',  '{B}'"
---
+++ util/concat_util.py
@@ -0,0 +1,6 @@
+def concat_4(A, B, C, D ):
+    return f"'{A}',  '{B}', '{C}', '{D}'"
+
+#print(concat('apple','banana', 'pear'))
+def concat_2(A, B ):
+    return f"'{A}',  '{B}'"
